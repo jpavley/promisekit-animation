31c368503bfc0d17f21d0c828583eb3c7489a948
John Pavley <jpavley@gmail.com>
2017-11-18 15:24:28 -0500

    initial commit

--- promisekit animation/ViewController.swift
+++ promisekit animation/ViewController.swift
@@ -7,12 +7,39 @@
 //
 
 import UIKit
+import PromiseKit
 
 class ViewController: UIViewController {
+    
+    let firstTag = 100
+    let cellCount = 9
 
+    var cellList = [UIView]()
+    
+    @IBOutlet weak var cell: UIView!
+    
+    @IBAction func animateAction(_ sender: Any) {
+        let fade = Promise()
+        for cell in tableView.visibleCells {
+            fade = fade.then {
+                UIView.promise(animateWithDuration:0.1) {
+                    cell.alpha = 0
+                }
+            }
+        }
+        fade.then {
+            //finish
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        for i in firstTag...firstTag+cellCount {
+            cellList.append(tag)
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
